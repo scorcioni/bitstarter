e2ba6326ab21cd2817767fb7b8f17a9150b35915
Ruggero <r.scorcioni@gmail.com>
2013-07-10 11:26:56 -0700

    hello index

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	//var in=fs.readFileSync("index.html");
+	var in=fs.readFileSync("index.html");
 
-  response.send('Hello World 2!');
+  response.send(in);
 });
 
 var port = process.env.PORT || 5000;
