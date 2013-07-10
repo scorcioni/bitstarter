07bb69cf202f5d73b64f035d12ed69a5bf96fbae
Ruggero <r.scorcioni@gmail.com>
2013-07-10 15:18:24 -0700

    index

--- web.js
+++ web.js
@@ -5,11 +5,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var index;
+  var buf=new Buffer(256);
   fs.readFileSync("index.html", function(err,data){
     if (err) throw err;
-    index=data;
+    index=buf.toString(data);
   });
-  response.send(data);
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
