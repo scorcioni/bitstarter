b8887d0911d17ff5b084181a444d6f20425fe957
Ruggero <r.scorcioni@gmail.com>
2013-07-22 12:32:02 -0700

    updated port 1001

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 1001;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
