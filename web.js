80441a4a7ae0a9f7909900edb7ac36608ae6c87e
Ruggero <r.scorcioni@gmail.com>
2013-07-22 12:55:32 -0700

    port 8080

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+
+var app=express();
 
 app.get('/', function(request, response) {
 
   response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
