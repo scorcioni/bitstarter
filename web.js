18b5e618a1a03fb29fc31c1241d863900ec1480a
Ruggero <r.scorcioni@gmail.com>
2013-07-22 12:33:28 -0700

    updated port 2001

--- web.js
+++ web.js
@@ -8,7 +8,6 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
 
-var port = process.env.PORT || 1001;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
