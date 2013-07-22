50481980a70fc79ebce78b7ded6c5a9275d81b7e
Ruggero <r.scorcioni@gmail.com>
2013-07-22 12:54:12 -0700

    port 3001

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync("index.html").toString());
 });
 
+var port = process.env.PORT || 3001;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
